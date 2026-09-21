b5d9c2451dbd6fbb1b5c98112c2ca65e245b996a
Priyank Deval <devalpriyank@gmail.com>
2026-09-21 16:09:17 +0530

    routing

--- http_module/tempCodeRunnerFile.js
+++ http_module/tempCodeRunnerFile.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import fs from 'fs'
 
 /*const server = http.createServer((req,res)=>{
     res.end("Welcome from server")
@@ -7,6 +8,10 @@ import http from 'http'
 server.listen(3000,()=>{
     console.log("Sever is running.....");
 })*/
+
+
+
+/*
 const server = http.createServer((req,res)=>{
     //res.end("Welcome from the server")
     if(req.url==="/"){
@@ -22,4 +27,27 @@ const server = http.createServer((req,res)=>{
 
 server.listen(3000,()=>{
     console.log("Sever is running.....");
-});    
+});*/
+
+const data=fs.readFileSync("./pageNotFound.html")
+
+
+ 
+const server = http.createServer((req, res)=>{
+    // res.end("welcome from server")
+    if(req.url === "/home"){
+        res.end("Home Page")
+    }else if(req.url === "/contact"){
+        res.end("Contact Page")
+    }else if(req.url === "/projects"){
+        res.end("Projects Page")
+    }else{
+        res.end(data)
+    }
+})
+
+server.listen(3000, ()=>{
+    console.log("Server is Running...")
+})
+
+
